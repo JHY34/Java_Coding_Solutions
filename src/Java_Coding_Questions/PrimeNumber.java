05195449750137a1deec28c804fefb3b6649b6b0
JHY34 <hamityilmaz34@gmail.com>
2020-12-23 16:40:07 -0600

    prime number examples

--- src/Java_Coding_Questions/PrimeNumber.java
+++ src/Java_Coding_Questions/PrimeNumber.java
@@ -2,22 +2,49 @@ package Java_Coding_Questions;
 
 public class PrimeNumber {
 
-    // What is a prime number?
-    // a number that is only divisible by itself and 1
+    public static void main(String[] args) {
 
-    public static boolean isPrimeNumber (int number) {
+        // What is a prime number?
+        // a number that is only divisible by itself and 1
 
-        for (int i=2; i<=number; i++) {
-            if (number % i == 0) {
-                return false;
+        // Let's test the solution
+        System.out.println(isPrimeNumber(10));  // prints: false
+        System.out.println(isPrimeNumber(13));  // prints: true
+        System.out.println(isPrimeNumber(37));  // prints: true
+        System.out.println(isPrimeNumber(50));  // prints: false
+
+        printPrimeNumbers(25);
+
+
+    }
+
+        public static boolean isPrimeNumber (int number) {
+
+            for (int i=2; i<=number/2; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;   // by default
+
+        }
+
+
+
+        // print prime numbers up to a number
+        public static void printPrimeNumbers (int num) {
+            for (int i = 2; i < num; i++) {
+                if ( isPrimeNumber(i)) {
+                    System.out.print(i + " ");
+                }
+
             }
         }
 
 
 
-        return true;   // by default
 
-    }
 
 
 
