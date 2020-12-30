e1231c3cc7a42b0478c68673552e4c46cb846cb0
JHY34 <hamityilmaz34@gmail.com>
2020-12-30 16:29:09 -0600

    palindrome code is solved

--- src/Java_Coding_Questions/PalindromeNumber.java
+++ src/Java_Coding_Questions/PalindromeNumber.java
@@ -2,10 +2,29 @@ package Java_Coding_Questions;
 
 public class PalindromeNumber {
 
+    public static void main(String[] args) {
+
+        // palindrome -> 1234321
+
+        System.out.println(isNumberPalindrome(123));    // false
+        System.out.println(isNumberPalindrome(12321));  // true
+    }
+
+
     public static boolean isNumberPalindrome (long number) {
         long reverse = 0;
+        long temp = number;
 
-        return
+        // using % 10 we can read the last digit
+
+        while (temp != 0) {
+            reverse = reverse * 10 + (temp % 10);  // 0 + 1 = 1
+                                                     // 10 + 2 = 12
+            temp /= 10 ;   // temp = temp / 10
+        }
+
+
+        return reverse == number;
     }
 
 }
