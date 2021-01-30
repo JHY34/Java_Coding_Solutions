8cce891dc0df2b192ceb9a040b3fcf0ca2122666
JHY34 <hamityilmaz34@gmail.com>
2021-01-30 15:44:06 -0600

    Second Highest Numbers Method is completed

--- src/Java_Coding_Questions/Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers.java
+++ src/Java_Coding_Questions/Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers.java
@@ -4,9 +4,9 @@ public class Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers {
 
     public static void main(String[] args) {
 
-        int[] arr1 = {3,5,10,2,56,-21};
+        int[] arr1 = {3,5,10,2,56,-21,15};
 
-        System.out.println(highestNumber(arr1));
+        System.out.println(secondHighestNumber(arr1));
 
     }
 
@@ -21,6 +21,25 @@ public class Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers {
         }
 
         return highestNumber;
+
+    }
+
+    public static int secondHighestNumber (int[] arr) {
+        int highestNumber = Integer.MIN_VALUE;
+        int secondHighestNumber = Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++) {
+
+            if (arr[i]> highestNumber) {
+                secondHighestNumber = highestNumber;
+                highestNumber = arr[i];
+            }
+            else if (arr[i]> secondHighestNumber) {
+                secondHighestNumber = arr[i];
+            }
+        }
+
+        return secondHighestNumber;
 
     }
 
