5a8d5d1dbb945b9b40296020c80b905618a62861
JHY34 <hamityilmaz34@gmail.com>
2021-01-30 17:23:08 -0600

    SumOfDigits Method is created

--- src/Java_Coding_Questions/String_SumOfDigits.java
+++ src/Java_Coding_Questions/String_SumOfDigits.java
@@ -3,9 +3,26 @@ package Java_Coding_Questions;
 public class String_SumOfDigits {
     public static void main(String[] args) {
 
+        String str = "abbbcc12345";
+
+        System.out.println("sumOfDigits(str) = " + sumOfDigits(str));
+
     }
 
-    public static int sumOfDigits () {
+    public static int sumOfDigits (String str) {
+
+        int sum = 0;
+
+        for (int i=0; i<str.length(); i++) {
+            // ascii table 48 = 0 ---> 57 = 9
+            if (str.charAt(i) >= 48 && str.charAt(i) <=57 ) {
+
+                sum += str.charAt(i) - 48;
+
+            }
+        }
+
+        return sum;
 
     }
 }
