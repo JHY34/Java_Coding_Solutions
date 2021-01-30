4c93f23b8952a710e79e7a99cd5d50f48fcb5989
JHY34 <hamityilmaz34@gmail.com>
2021-01-30 15:36:18 -0600

    Highest Numbers Method is completed

--- src/Java_Coding_Questions/Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers.java
+++ src/Java_Coding_Questions/Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers.java
@@ -1,4 +1,29 @@
 package Java_Coding_Questions;
 
 public class Array_Finding_Highest_SecondHighest_ThirdHighest_Numbers {
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {3,5,10,2,56,-21};
+
+        System.out.println(highestNumber(arr1));
+
+    }
+
+    public static int highestNumber (int[] arr) {
+        int highestNumber = Integer.MIN_VALUE;
+
+        for (int i=0; i<arr.length; i++) {
+
+            if (arr[i]> highestNumber) {
+                highestNumber = arr[i];
+            }
+        }
+
+        return highestNumber;
+
+    }
+
 }
+
+
